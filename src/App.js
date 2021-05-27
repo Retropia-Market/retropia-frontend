567d22714333c40be3f0a8fd4468f6159c849277
Germán Abenza <gabenza95@gmail.com>
2021-05-27 12:48:18 +0200

    logica navbar, redux y router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
+
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          Home
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,61 @@
+import { useState } from "react"
+import { Link, useHistory } from "react-router-dom"
+
 import logo from '../img/logo.svg';
 import '../styles/Navbar.css';
 
+function Categories({categories, setIndex}){
+  const hoverHandler = i => {
+    setIndex(i)
+  }
+  return( 
+    <div className="categories">
+      {categories.map((c, i) => <Link to={'/catalogue/' + c} Link key={i} onMouseEnter={() => hoverHandler(i)}>{c}</Link>)}
+    </div>
+  )
+}
+
+function Subcategories({index}){
+  return <>
+    {index === 0 && <>
+      subcategorias consolas
+    </>}
+    {index === 1 && <>
+      subcategorias videojuegos
+    </> }
+    {index === 2 && <>
+      subcategorias perifericos
+    </> }
+  </>
+}
+
 const Navbar = () => {
+  const [index, setIndex] = useState(0)
+  const [search, setSearch] = useState('')
+  const history = useHistory()
+  const categories = ['consolas', 'videojuegos', 'perifericos']
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    history.push('catalogue/' + search)
+  }
+
   return (
     <div className="navbar-container">
       <div className="logo">
         <img src={logo} alt="Logo Retropia Market" />
       </div>
-      <div className="right-navbar">
+      <nav className="right-navbar">
         <div className="upper">
           <div className="search">
-            <div className="search-bar">
+            <form className="search-bar" onChange={e=> setSearch(e.target.value)}>
               <input type="text" placeholder="Ingresa tu búsqueda..." />
               <button>🔍</button>
-            </div>
-            <div className="categories">
-              <div className="consolas">CONSOLAS</div>
-              <div className="videogames">VIDEOJUEGOS</div>
-              <div className="gadgets">PERIFÉRICOS</div>
-            </div>
+            </form>
+            <Categories categories={categories} setIndex={setIndex} onSubmit={submitHandler}/>
           </div>
           <div className="user-nav">
-            <div className="sell-button">VENDER +</div>
+            <Link className="sell-button">VENDER +</Link>
             <div className="messages">ICON</div>
             <div className="notifications">ICON</div>
             <div className="user">
@@ -30,8 +64,10 @@ const Navbar = () => {
             </div>
           </div>
         </div>
-        <div className="lower"></div>
-      </div>
+        <div className="lower">
+          <Subcategories index={index} />
+        </div>
+      </nav>
     </div>
   );
 };
