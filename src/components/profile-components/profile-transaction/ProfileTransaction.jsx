7d5dc8829202ead26a1e8b90a049cad1a8602777
Renzocdd <57793035+Renzocdd@users.noreply.github.com>
2021-06-03 21:53:01 +0200

    Resposivness Tweaks

--- src/components/profile-components/profile-transaction/ProfileTransaction.jsx
+++ src/components/profile-components/profile-transaction/ProfileTransaction.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import { useState } from 'react';
 
 function ProfileTransaction() {
-  const [finishedSales, setFinishedSales] = useState(false);
+  const [finishedSales, setFinishedSales] = useState('activas');
   const [finishedBids, setFinishedBids] = useState(false);
   const user = useSelector((s) => s.user);
 
@@ -15,28 +15,26 @@ function ProfileTransaction() {
   return (
     <div className="transactions-main">
       <ul className="transactions-navbar">
-        <li className="transactions-navbar-item">
-          <NavLink to="/profile/transactions">
+        <NavLink to="/profile/transactions">
+          <li className="transactions-navbar-item">
             <h2>Ventas</h2>
-          </NavLink>
-        </li>
-        <li className="transactions-navbar-item">
-          <NavLink to="/profile/transactions/bids">
+          </li>
+        </NavLink>
+        <NavLink to="/profile/transactions/bids">
+          <li className="transactions-navbar-item">
             <h2>Compras</h2>
-          </NavLink>
-        </li>
+          </li>
+        </NavLink>
       </ul>
       <div className="transactions-section">
         <Switch>
           <Route path="/profile/transactions" exact>
             <h1>Ventas</h1>
-            <div className="sub-sections-nav">
-              <h3 onClick="">Activas</h3>
-              <h3>Finalizadas</h3>
-            </div>
+            {/* <TransactionSell /> */}
           </Route>
           <Route path="/profile/transactions/bids" exact>
             <h1>Compras & ofertas</h1>
+            {/* <TransactionBid /> */}
           </Route>
           <Route path="/profile/transactions">Not Found</Route>
         </Switch>
