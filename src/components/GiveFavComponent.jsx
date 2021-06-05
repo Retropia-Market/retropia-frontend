d9c3fa65bd2ccda6223db09ee1c9b08a44d0eb4d
Renzocdd <57793035+Renzocdd@users.noreply.github.com>
2021-06-05 22:11:16 +0200

    Styles Teaks

--- src/components/GiveFavComponent.jsx
+++ src/components/GiveFavComponent.jsx
@@ -1,61 +1,68 @@
-import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
-import useFetch from "../hooks/useFetch"
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import useFetch from '../hooks/useFetch';
 
 import colorHeart from '../img/icons/heart-color-icon.svg';
 import greyHeart from '../img/icons/heart-grey-icon.svg';
 
-const GiveFavComponent = ({ data}) => {
+const GiveFavComponent = ({ data }) => {
+  const user = useSelector((s) => s.user);
 
-    const user = useSelector(s => s.user)
+  const apiFavURL = `http://localhost:8080/${user.userData.id}/getFavourites/`;
 
-  const apiFavURL = `http://localhost:8080/${user.userData.id}/getFavourites/`
+  const [favorites] = useFetch(apiFavURL, user);
+  const [fav, setFav] = useState(false);
 
-  const [favorites] = useFetch(apiFavURL, user)
-  const [fav, setFav] = useState(false)
-
-  
   useEffect(() => {
-      if(favorites){
-          const getFav = favorites.map(prod => prod.id)
-          getFav.forEach(id => {
-              if(id === data.id) setFav(true)
-            })
+    if (favorites) {
+      const getFav = favorites.map((prod) => prod.id);
+      getFav.forEach((id) => {
+        if (id === data.id) setFav(true);
+      });
+    }
+  }, [favorites, data.id]);
+
+  const handleFav = async () => {
+    if (!fav) {
+      const ret = await fetch(
+        `http://localhost:8080/${data.id}/addFavourite/${user.userData.id}`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: 'Bearer ' + user.token,
+          },
         }
-        
-        
-    },[favorites, data.id])
+      );
+      if (ret.ok) {
+        setFav(true);
+      }
+    } else {
+      const ret = await fetch(
+        `http://localhost:8080/${data.id}/removeFavourite/${user.userData.id}`,
+        {
+          method: 'DELETE',
+          headers: {
+            Authorization: 'Bearer ' + user.token,
+          },
+        }
+      );
+      if (ret.ok) {
+        console.log(ret);
+        setFav(false);
+      }
+    }
+  };
+  return (
+    <div
+      className="heart-icon"
+      onClick={handleFav}
+      style={{
+        background: fav
+          ? `url(${colorHeart}) no-repeat`
+          : `url(${greyHeart}) no-repeat`,
+      }}
+    ></div>
+  );
+};
 
-    const handleFav = async() => {
-        if(!fav){
-            const ret = await fetch(`http://localhost:8080/${data.id}/addFavourite/${user.userData.id}`, {
-                  method: 'POST',
-                  headers : {
-                      'Authorization' : 'Bearer ' + user.token,
-                  },
-                })
-             if(ret.ok){
-            setFav(true)  
-        }}
-        else {
-            const ret = await fetch(`http://localhost:8080/${data.id}/removeFavourite/${user.userData.id}`, {
-                  method: 'DELETE',
-                  headers : {
-                      'Authorization' : 'Bearer ' + user.token,
-                  },
-                })
-             if(ret.ok){
-                 console.log(ret)
-            setFav(false) 
-        
-     }
-     
-   
-        }}
-    return (
-            <div className="heart-icon" onClick={handleFav} style={{backgroundImage : fav ? `url(${colorHeart})` : `url(${greyHeart})`}}>
-            </div>
-    )
-}
-
-export default GiveFavComponent
+export default GiveFavComponent;
