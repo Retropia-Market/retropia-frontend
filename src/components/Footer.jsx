7ffe19e505380e356e68411bd76f922fb0166cae
Renzo <renzomondoconv@gmail.com>
2021-06-11 22:11:12 +0200

    Footer Creado

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="links">LINKS</div>
+      <div className="footer-logo">LOGO</div>
+      <div className="social-links">LINKS SOCIAL</div>
+    </div>
+  );
+};
+
+export default Footer;
