0093428867edcdd9faed428f559471dfd107fe33
Ivan Fernandez <ivan.ferndz94@gmail.com>
2021-06-21 22:41:39 +0200

    estilos new sale

--- src/components/ProductsComponents/NewSaleComponents/NameConsoleSelector.jsx
+++ src/components/ProductsComponents/NewSaleComponents/NameConsoleSelector.jsx
@@ -1,27 +1,44 @@
-import SelectSearch from "react-select-search";
+import SelectSearch from 'react-select-search';
 
-const NameConsoleSelector = ({setProductName}) => {
+const NameConsoleSelector = ({ setProductName }) => {
     const getFunOp = (query) => {
-        if(!query) return []
-            return new Promise((resolve, reject) => {
-                     fetch(`http://localhost:8080/rawg/platform`)
-                        .then(response => response.json())
-                        .then(( {results} ) => {
-                            if(results) {
-                                resolve(results.filter((console,i)=> console.name.toLowerCase().indexOf(query.toLowerCase()) !== -1).map((product, i) => ({ value: [product, i], name: product.name })))
-                            } 
-                           })
-                        .catch(reject);
-            });
-}
-        
+        if (!query) return [];
+        return new Promise((resolve, reject) => {
+            fetch(`http://localhost:8080/rawg/platform`)
+                .then((response) => response.json())
+                .then(({ results }) => {
+                    if (results) {
+                        resolve(
+                            results
+                                .filter(
+                                    (console, i) =>
+                                        console.name
+                                            .toLowerCase()
+                                            .indexOf(query.toLowerCase()) !== -1
+                                )
+                                .map((product, i) => ({
+                                    value: [product, i],
+                                    name: product.name,
+                                }))
+                        );
+                    }
+                })
+                .catch(reject);
+        });
+    };
 
-return (
-    <div className="new-sale-selector">
-          <SelectSearch options={[]} getOptions={getFunOp} search
-        placeholder="Nombre"  debounce={1000}  onChange={setProductName} />
-    </div>
-)
-}
+    return (
+        <div className="new-sale-selector">
+            <SelectSearch
+                options={[]}
+                getOptions={getFunOp}
+                search
+                placeholder="Nombre de tu consola"
+                debounce={1000}
+                onChange={setProductName}
+            />
+        </div>
+    );
+};
 
-export default NameConsoleSelector
+export default NameConsoleSelector;
