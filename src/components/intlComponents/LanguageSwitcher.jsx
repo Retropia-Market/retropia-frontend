2eaac757f3caad4b217487637cf47401746ac4f0
Renzocdd <57793035+Renzocdd@users.noreply.github.com>
2021-06-22 12:58:58 +0200

    Agregado idioma italiano

--- src/components/intlComponents/LanguageSwitcher.jsx
+++ src/components/intlComponents/LanguageSwitcher.jsx
@@ -1,22 +1,22 @@
 import es from '../../locale/es.json';
 import en from '../../locale/en.json';
-import it from '../../locale/en.json';
+import it from '../../locale/it.json';
 import gl from '../../locale/gl.json';
 import { IntlProvider } from 'react-intl';
 import { useSelector } from 'react-redux';
 const allMessages = { es, en, gl, it };
 
 const LanguageSwitcher = ({ children }) => {
-    const { language } = useSelector((s) => s.language);
-    return (
-        <IntlProvider
-            messages={allMessages[language]}
-            locale={language}
-            defaultLocale="es"
-        >
-            {children}
-        </IntlProvider>
-    );
+  const { language } = useSelector((s) => s.language);
+  return (
+    <IntlProvider
+      messages={allMessages[language]}
+      locale={language}
+      defaultLocale="es"
+    >
+      {children}
+    </IntlProvider>
+  );
 };
 
 export default LanguageSwitcher;
