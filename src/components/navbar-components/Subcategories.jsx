7ea0d815ded828fcbacaecede0a9e7412057fe61
Germán Abenza <gabenza95@gmail.com>
2021-06-23 12:23:01 +0200

    modificado padding subcategorias

--- src/components/navbar-components/Subcategories.jsx
+++ src/components/navbar-components/Subcategories.jsx
@@ -22,7 +22,7 @@ const Subcategories = ({ categoryIndex, hideSubcategories, showCategories, type 
     <FontAwesomeIcon className="category-select" icon={faChevronLeft} onClick={handleClick}></FontAwesomeIcon>
     {!allCategories && <li>cargando...</li> }
     {allCategories?.filter(c => c.category_id === categoryIndex).map(c => 
-      <li className="category" key={c.id} onClick={(e) => handleClickSubcategories(c)}>
+      <li className="category subcategory" key={c.id} onClick={(e) => handleClickSubcategories(c)}>
         <div className="category-name" >{c.name}</div>
       </li>
       )}
